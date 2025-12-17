21b7663823704962448190fe839d62a5e6a4f7e9
jh-sang <36220174+jh-sang@users.noreply.github.com>
2025-12-17 23:44:01 +0900

    Update behavior_rgb_ug_solid_breathing.c

--- modules/behaviors/behavior_rgb_ug_solid_breathing.c
+++ modules/behaviors/behavior_rgb_ug_solid_breathing.c
@@ -17,7 +17,7 @@ static int behavior_rgb_ug_solid_breathing_binding_pressed(struct zmk_behavior_b
     // 내부에서 상태를 기억합니다. 
     // 키보드를 처음 켰을 때와 싱크가 안 맞으면 한 번 더 누르면 됩니다!
     static bool is_breathing = false;
-    return zmk_rgb_underglow_cycle_effect(1); /*
+    return zmk_rgb_underglow_select_effect(1); /*
     if (is_breathing) {
         is_breathing = false;
         return zmk_rgb_underglow_select_effect(EFFECT_SOLID);
@@ -32,13 +32,12 @@ static int behavior_rgb_ug_solid_breathing_binding_released(struct zmk_behavior_
     return 0; // 뗐을 때는 아무 동작 안 함
 }
 
-/* [핵심] 최신 ZMK 멤버 이름인 on_key_param_... 사용 */
 static const struct zmk_behavior_api behavior_rgb_ug_solid_breathing_api = {
     .on_key_param_pressed = behavior_rgb_ug_solid_breathing_binding_pressed,
     .on_key_param_released = behavior_rgb_ug_solid_breathing_binding_released,
 };
 
-/* ZMK에서 가장 권장하는 표준 인스턴스 생성 방식입니다. */
+// 인스턴스 생성
 #define RGB_UG_INST(n) \
     BEHAVIOR_DT_INST_DEFINE(n, \
                             behavior_rgb_ug_solid_breathing_init, \
@@ -49,5 +48,4 @@ static const struct zmk_behavior_api behavior_rgb_ug_solid_breathing_api = {
                             CONFIG_ZMK_BEHAVIOR_INIT_PRIORITY, \
                             &behavior_rgb_ug_solid_breathing_driver_api);
 
-/* dts에 선언된 모든 해당 compatible 노드에 대해 위 매크로를 실행합니다. */
 DT_INST_FOREACH_STATUS_OKAY(RGB_UG_INST)
